d6598afee90782f29d4221dcfbe3599a00432b6e
Avnish <com.avnish@gmail.com>
2025-09-30 22:53:37 +0530

    Code Updated

--- etawah/shop/admin.py
+++ etawah/shop/admin.py
@@ -1,6 +1,9 @@
-from django.contrib import admin # pyright: ignore[reportMissingModuleSource]
+from django.contrib import admin 
 from .models import Feature, PricingPlan
 
 admin.site.register(Feature)
 admin.site.register(PricingPlan)
+admin.site.site_header = "Etawah Shops Admin"
+admin.site.site_title = "Etawah Shops Admin Portal"
+admin.site_index_title = "Welcome to Etawah Shops Admin"
 
